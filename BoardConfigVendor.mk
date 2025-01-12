00244878af08d9cfc90396f6311c0cdff0106675
srgrusso <srgrusso240@gmail.com>
2025-01-12 16:24:29 -0600

    Revert "sunfish: Import firmware from TQ3A.230805.001.S1"
    This reverts commit 733c6c6d35122b9553206f72997afd6ea9d35a8c.

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -2,15 +2,3 @@
 # Automatically generated file. DO NOT MODIFY
 #
 
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    devcfg \
-    hyp \
-    keymaster \
-    modem \
-    qupfw \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
